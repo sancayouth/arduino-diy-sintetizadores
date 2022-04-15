f9f6bf478a7278ddfe8e9a87edcad76030bec090
SancaYouth <cccsarmiento@gmail.com>
2022-04-15 18:01:02 -0300

    Update ejemplo 1

--- 01_cambia_frecuencia_potenciometro/01_cambia_frecuencia_potenciometro.ino
+++ 01_cambia_frecuencia_potenciometro/01_cambia_frecuencia_potenciometro.ino
@@ -1,20 +1,22 @@
 /*
 
   circuito:
-  - Potenciometro de 10k con el pin central conectado en el pin analogo 0
+  - Potenciometro de 10k con el pin central conectado en el pin analogo 0,
+    los pin de los lados van conectados a +5V y tierra
   - Salida de audio conectada al pin digital 9
 
 */
 
-void setup() {
-  // Inicializamos el serial para debuggear 
+void setup()
+{
+  // Inicializamos el serial para debuggear
   Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
   // Leemos el potenciometro
   int pot_frequencia = analogRead(A0);
-  //Serial.println(pot_frequencia);
   int pitch = map(pot_frequencia, 0, 1023, 220, 880);
   Serial.println(pitch);
 
